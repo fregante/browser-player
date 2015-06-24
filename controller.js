3a9c89816346c9d2e1c2b9844989525ba0347402
Federico Brigante <bfred-it@users.noreply.github.com>
2015-06-24 22:00:30 +0900

    Wait a second before playing
    Part of #3

--- controller.js
+++ controller.js
@@ -1,17 +1,6 @@
 'use strict';
 /*global tabs, debounce*/
 
-function handlePlay (tab) {
-	tabs.remove(tab.id); // prevent duplicates and pausing oneself
-	tabs.last.act('pause');
-	tabs.add(tab);
-}
-function handlePause (tab) {
-	var wasRemoved = tabs.remove(tab.id || tab);
-	if (wasRemoved) {
-		tabs.last.act('resume');
-	}
-}
 function enableAllCurrentTabs () {
 	var scripts = chrome.runtime.getManifest().content_scripts;
 	scripts.forEach(function (script) {
@@ -34,18 +23,39 @@ function enableAllCurrentTabs () {
 
 var media = {};
 
+media.shouldPlayNext = false;
+media.handlePlay = function (tab) {
+	tabs.remove(tab.id); // prevent duplicates and pausing oneself
+	tabs.last.act('pause');
+	tabs.add(tab);
+
+	media.shouldPlayNext = false;
+};
+
+media.handlePause = function (tab, skipTimer) {
+	var wasRemoved = tabs.remove(tab.id || tab);
+	if (wasRemoved) {
+		media.shouldPlayNext = true;
+		setTimeout(function () {
+			if (media.shouldPlayNext) {
+				tabs.last.act('resume');
+			}
+		}, skipTimer?0:1000);
+	}
+};
+
 media.hasQueuedPause = false;
 media.queuePause = debounce(function (tab) {
 	if (media.hasQueuedPause) {
 		console.log('pause');
-		handlePause(tab);
+		media.handlePause(tab, true);
 	}
 }, 1000); // give it time to seek, if the pause is caused by that
 
 media.onplay = function (request, sender) {
 	media.hasQueuedPause = false;
 	console.log('play');
-	handlePlay(sender.tab);
+	media.handlePlay(sender.tab);
 };
 
 media.onpause = function (request, sender) {
@@ -66,6 +76,6 @@ chrome.runtime.onMessage.addListener(function (request, sender) {
 		// console.log('tabs.list:', tabs.list);
 	}
 });
-chrome.tabs.onRemoved.addListener(handlePause);
+chrome.tabs.onRemoved.addListener(media.handlePause);
 setInterval(tabs.keepUpdated, 3000);
 enableAllCurrentTabs();
